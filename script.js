98a32f42d5e65f79512aed5a183e5407156c0e67
Michele291123 <152420730+Michele291123@users.noreply.github.com>
2023-11-30 17:15:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let numero = document.getElementById("numero")
-
-function scrivi() {
-    numero.textContent = genera()
-}
+let min, max
 
 function genera() {
-    let min = Number(document.getElementById("n-min").value)
-    let max = Number(document.getElementById("n-max").value)
+    min = Math.max(Number(document.getElementById("n-min").value), 0);
+    max = Math.max(Number(document.getElementById("n-max").value), 0);
     let x = Math.floor(((Math.random() * max) / min) +1)
     return x
+}
+
+function scrivi() {
+    numero.textContent = genera() 
 }
